608e19aa579214cb2cf81efd2b4afcd2804874cf
Mikhail Chernyshev <lishainik@gmail.com>
2020-05-27 03:24:31 +0300

    added js and css stuff

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,13 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
+},
+module: {
+    rules: [{ 
+        test: /\.js$/, 
+        use: { loader: "babel-loader" },
+        exclude: /node_modules/ 
+            }
+        ]
 }
 }
